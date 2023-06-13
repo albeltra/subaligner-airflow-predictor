fb49ce5a21588cd98683bb5262045d7f0ff15e61
albeltra <alexbeltran@berkeley.edu>
2023-06-13 15:58:00 -0700

    update to module predictor call

--- predict.py
+++ predict.py
@@ -2,20 +2,12 @@ import ast
 import subprocess
 import os
 from pathlib import Path
+from subaligner.predictor import Predictor
 
-media_path = ast.literal_eval(os.environ.get('mediaFile'))['path'] 
+media_path = ast.literal_eval(os.environ.get('mediaFile'))['path']
 
 subtitle_path = "/TEMP-SUBS/" + Path(media_path).name + '.srt'
 audio_file_path = "/TEMP-SUBS/" + Path(media_path).name + '.wav'
 
-command = ["subaligner",
-           "-m",
-           'single',
-           "-v",
-           media_path,
-           "-s",
-           subtitle_path,
-           "-a",
-           audio_file_path,]
-
-subprocess.run(command, check=True)
+predictor = Predictor()
+predictor.predict_single_pass(subtitle_file_path=subtitle_path, audio_file_path=audio_file_path) 
