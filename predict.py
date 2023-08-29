2aa45eccfbae8872c32f22b31e7a0e001f160d3c
albeltra <alexbeltran@berkeley.edu>
2023-08-29 16:34:28 -0700

    update submodule

--- predict.py
+++ predict.py
@@ -4,11 +4,11 @@ from pathlib import Path
 
 from subaligner.predictor import Predictor
 
-media_path = ast.literal_eval(os.environ.get('mediaFile'))['path']
+media_path = "King.Richard.2021.1080p.BluRay.x264.DTS-HD.MA.7.1-MT.mkv"
 
 subtitle_path = "/audio-subs/" + Path(media_path).name
 audio_file_path = "/audio-subs/" + Path(media_path).name + '.wav'
-video_file_path = "/data/v4/" + Path(media_path).name 
+video_file_path = "/data/v4/" + Path(media_path).name
 
 predictor = Predictor()
 predictor.predict_single_pass(video_file_path=video_file_path,
